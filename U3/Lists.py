6df91a02dd3f072ba22445d05b24f53fd814ecac
UP210356 <up210356@alumnos.upa.edu.mx>
2023-04-02 16:32:36 -0600

    Lists 1

--- U3/Lists.py
+++ U3/Lists.py
@@ -6,20 +6,71 @@ class Node:
     def getData(self):
         return self.data
     
+    def getNext(self):
+        return self.next
+    
+    def asignData(self, next):
+        self.data = next
+
+    def asignNext(self, new):
+        self.next = new
+    
 class List:
     def insert(self, data):
-        pass
+        act = self.cabeza
+        prev = None
+        detenerse = False
+        while act != None and not detenerse:
+            if act.getData() > data:
+                detenerse = True
+            else:
+                prev = act
+                act = act.getNext()
+
+        temp = Node(data)
+        if prev == None:
+            temp.asignNext(self.cabeza)
+            self.cabeza = temp
+        else:
+            temp.asignNext(act)
+            prev.asignNext(temp)
+            
     def deldat(self, data):
         pass
+
     def delpos(self, pos):
         pass
+
     def getSize(self):
         pass
+
     def search(self, data):
-        pass
+        actual = self.head
+        found = False
+        stop = False
+        while actual != None and not found and not stop:
+            if actual.obtenerDato() == data:
+                encontrado = True
+            else:
+                if actual.obtenerDato() > data:
+                    detenerse = True
+                else:
+                    actual = actual.obtenerSiguiente()
+
+        return encontrado
+    
     def seek(self, pos):
         pass
+
     def printIzq(self):
         pass
+
     def printDer(self):
         pass
+
+
+q = List()
+q.insert(56)
+q.insert(2)
+q.insert(34)
+print(q.show())
